ab0fe903fe1a284d48b1c9105dd748a10622c99b
minh <minhminh140105@gmail.com>
2021-08-01 18:14:36 +0700

    hu

---
+++ adu.py
@@ -0,0 +1,4 @@
+a =input("nhap 1 so be hon 10:")
+for number in a:
+    b+=1
+    print(range(1,b))
---
+++ buoi4.py
@@ -0,0 +1,25 @@
+a=[1,2,3,4,5,6,7]
+b=0
+for number in a:
+    b+=number 
+print(b)
+# length=0
+# for number in a:
+#     length+=1
+# print(length)
+# range(start, stop, step) tao ra cac so bang cach + step tu start den truoc stop
+# print(list(range(6)))
+# print(list(range(-3,8,1)))
+# print(list(range(3, -4.-1)))
+# for i in range(6):
+#     print(i)
+# animals=['dog', 'cat', 'cow', 'fish']
+# for i in range(len(animals)):
+#     print(animals[i])
+
+# for number in range(2,7):
+#     print(list(range(1, number)))
+# a=[10,20,30,40,50,60]
+# length= len(a)
+# for i in range(-1,-length-1,-1):
+#     print(a[i])
